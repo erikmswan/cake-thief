525b6fd0ecabf9e14725de88dad5a2f5bb68d21d
Erik Michael Swan <erikmswan@gmail.com>
2017-06-16 14:02:57 -0700

    build models

--- dist/models.js
+++ dist/models.js
@@ -8,8 +8,8 @@ Object.defineProperty(exports, "__esModule", {
 // input with edge case of 0 value cakes
 
 var cakes = {
-    capacity: 40.5,
-    cakes: [{ weight: 2.4, value: 60 }, { weight: 9.6, value: 50 }, { weight: 4.1, value: 40 }, { weight: 8.9, value: 20 }, { weight: 3, value: 70 }, { weight: 20.1, value: 0 }, { weight: 15, value: 0 }, { weight: 13, value: 0 }]
+    capacity: 30,
+    cakes: [{ weight: 2, value: 60 }, { weight: 9, value: 50 }, { weight: 4, value: 40 }, { weight: 8, value: 20 }, { weight: 3, value: 70 }, { weight: 20, value: 0 }, { weight: 15, value: 0 }, { weight: 13, value: 0 }]
 };
 
 // input with edge case of 0 weight cakes
