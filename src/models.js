4dfafdd0af65d3fe26b56f7b756c4be1b5111d3c
Erik Michael Swan <erikmswan@gmail.com>
2017-06-16 14:00:20 -0700

    woops, data is supposed to be only integers

--- src/models.js
+++ src/models.js
@@ -7,14 +7,14 @@ import type { Input } from './types';
 // input with edge case of 0 value cakes
 
 const cakes: Input = {
-    capacity : 40.5,
+    capacity : 30,
     cakes    : [
-        { weight: 2.4, value: 60 },
-        { weight: 9.6, value: 50 },
-        { weight: 4.1, value: 40 },
-        { weight: 8.9, value: 20 },
+        { weight: 2, value: 60 },
+        { weight: 9, value: 50 },
+        { weight: 4, value: 40 },
+        { weight: 8, value: 20 },
         { weight: 3, value: 70 },
-        { weight: 20.1, value: 0 },
+        { weight: 20, value: 0 },
         { weight: 15, value: 0 },
         { weight: 13, value: 0 }
     ]
